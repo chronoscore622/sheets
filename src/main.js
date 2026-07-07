998b99e320595580dca3b79cb74044936d2f2788
leaf_wiki <chronos.core46@gmail.com>
2026-07-07 23:42:58 +0200

    action

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+try {
+  const key = core.getInput("key");
+  const id = core.getInput("spreadsheetId");
+  const sheet = core.getInput("sheetName");
+  const link = `https://sheets.googleapis.com/v4/spreadsheets/${id}/values/${sheet}?key=${key}`;
+  
+  fetch(link)
+    .then(response => response.json())
+    .then(data => core.setOutput("result", data.values))
+    .catch(error => console.error('Error:', error))
+  ;
+
+  core.info(`Completed (probably)`);
+} catch (error) {
+  core.setFailed(error.message);
+};
