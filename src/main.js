69532540232ac0a8f4f555831ddaaf1d5a39e479
leaf <145163317+astesicelive@users.noreply.github.com>
2026-07-08 01:54:43 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,18 +2,8 @@ import * as core from "@actions/core";
 import * as github from "@actions/github";
 
 try {
-  const key = core.getInput("key");
-  const id = core.getInput("spreadsheetId");
-  const sheet = core.getInput("sheetName");
-  const link = `https://sheets.googleapis.com/v4/spreadsheets/${id}/values/${sheet}?key=${key}`;
-  
-  fetch(link)
-    .then(response => response.json())
-    .then(data => core.info(data.values))
-    .catch(error => console.error('Error:', error))
-  ;
-
-  core.setOutput("result", 'testing');
+  const data = core.getInput("data");
+  core.setOutput("result", JSON.stringify(data));
 } catch (error) {
   core.setFailed(error.message);
 };
